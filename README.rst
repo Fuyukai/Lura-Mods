e010e47c4f755a6f99f7b5cda2f88634801645a0
Lura Skye <l@veriny.tf>
2023-07-13 14:38:15 +0100

    publishing + licence

--- README.rst
+++ README.rst
@@ -4,6 +4,23 @@ Lura Mods
 This is a small handful of quick-n-dirty Fabric mods that I made for things I needed. Just
 build them locally and bundle them directly into your pack's mrpack.
 
+All of these are available on my Maven:
+
+.. code-block:: kotlin
+
+    allprojects {
+        repositories {
+            maven("https://maven.veriny.tf/releases") {
+                mavenContent {
+                    includeGroupAndSubgroups("tf.veriny")
+                }
+            }
+
+            // may be needed for some dependencies
+            maven("https://maven.veriny.tf/mirror")
+        }
+    }
+
 Tag View
 --------
 
---
+++ lura-tagview/build.gradle.kts
@@ -0,0 +1,28 @@
+version = "1902.0.0"
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+
+        pom {
+            name.set(project.name)
+            description.set("Tag Viewer")
+            url.set("https://github.com/Fuyukai/")
+
+            licenses {
+                license {
+                    name.set("MPL-2.0")
+                    url.set("https://www.mozilla.org/en-US/MPL/2.0/")
+                }
+            }
+
+            developers {
+                developer {
+                    id.set("Fuyukai")
+                    name.set("Lura Skye")
+                    url.set("https://veriny.tf")
+                }
+            }
+        }
+    }
+}
